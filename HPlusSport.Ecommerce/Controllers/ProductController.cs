fa2ea93b1cb6a4505a90363d9f7d76c0d736dbe0
Michel Nsimba <nsimba_matondo@hotmail.com>
2021-03-06 20:35:01 +0100

    Modifying the Product controller to use ViewBag to convey data to the view

--- HPlusSport.Ecommerce/Controllers/ProductController.cs
+++ HPlusSport.Ecommerce/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HPlusSport.Ecommerce.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,13 +12,23 @@ namespace HPlusSport.Ecommerce.Controllers
         // GET: Product
         public ActionResult Index()
         {
-            return View();
+            return View("Index");
         }
 
         [Route("product/{productName}")]
         public ActionResult Detail(string productName)
         {
             // lookup the product name in the database
+            ViewBag.Product = new Product
+            {
+                Name = "Women winter's jacket",
+                FullPrice = 20.00M,
+                CurrentPrice = 16.00M,
+                PercentOff = 20,
+                ImagePath = "/Content/Images/Products/1.jpg",
+                StarRating = 4
+            };
+
             return View();
         }
     }
